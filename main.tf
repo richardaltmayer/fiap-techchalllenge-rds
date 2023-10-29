8719e6ff1d2a0a4da606b4fcb755cfff50bac94c
Richard Altmayer <richardmateus@gmail.com>
2023-10-29 16:28:26 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
   cloud {
     organization = "fiap_techchallenge"
 
